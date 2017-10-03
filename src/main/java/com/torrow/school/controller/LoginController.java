0b99b495abc9503d8d0058c6007952e6c9a8939e
zhangjingao <1763608200@qq.com>
2017-10-03 16:40:16 +0800

    update

--- src/main/java/com/torrow/school/controller/LoginController.java
+++ src/main/java/com/torrow/school/controller/LoginController.java
@@ -9,9 +9,10 @@ import com.torrow.school.base.BaseController;
 import com.torrow.school.entity.TbUser;
 
 /**
- * @author 寮犻噾楂�
+ * 
+ * @author 张金高
  *
- * 2017骞�10鏈�2鏃ヤ笂鍗�9:10:26
+ * 2017年10月3日下午4:36:56
  */
 @Controller
 @RequestMapping("/")
@@ -31,12 +32,9 @@ public class LoginController extends BaseController {
 	@RequestMapping("login")
 	public String login(String usEmail,String usPassword,Model model){
 		
-		System.out.println("用户登录："+usEmail+usPassword);
-		
 		TbUser tbUser=userService.login(usEmail,usPassword);
 		if(tbUser!=null) {
 			model.addAttribute("msg", "登录成功");
-			
 		}else{
 			model.addAttribute("msg", "用户名或密码错误");
 		}
