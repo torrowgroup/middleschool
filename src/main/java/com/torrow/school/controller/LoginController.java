8ef0f1f5f62732544c45c1a01f34e9226789df9e
anlijie <2954811062@qq.com>
2017-10-04 15:10:35 +0800

    登录的修改bug

--- src/main/java/com/torrow/school/controller/LoginController.java
+++ src/main/java/com/torrow/school/controller/LoginController.java
@@ -6,9 +6,10 @@ import com.torrow.school.base.BaseController;
 import com.torrow.school.entity.TbUser;
 
 /**
- * @author 寮犻噾楂�
+ * 
+ * @author 张金高
  *
- * 2017骞�10鏈�2鏃ヤ笂鍗�9:10:26
+ * 2017年10月3日下午4:36:56
  */
 @Controller
 @RequestMapping("/")
@@ -27,8 +28,6 @@ public class LoginController extends BaseController {
 	 */
 	@RequestMapping("login")
 	public String login(String usEmail,String usPassword,Model model){
-		
-		System.out.println("用户登录："+usEmail+usPassword);
 		
 		TbUser tbUser=userService.login(usEmail,usPassword);
 		if(tbUser!=null) {
