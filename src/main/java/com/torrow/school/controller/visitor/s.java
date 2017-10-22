cb8f52296f7c81906fe8cbc8e6cfdbb4320c3f01
zhangjingao <1763608200@qq.com>
2017-10-22 09:50:50 +0800

    update bug

---
+++ src/main/java/com/torrow/school/controller/visitor/s.java
@@ -0,0 +1,13 @@
+
+package com.torrow.school.controller.visitor;
+
+/**
+ * @author 张金高
+ *
+ * 2017年10月22日上午9:23:58
+ */
+public class s {
+
+	
+	
+}
--- src/main/java/com/torrow/school/service/TbUserService.java
+++ src/main/java/com/torrow/school/service/TbUserService.java
@@ -1,37 +1,44 @@
 package com.torrow.school.service;
+
 import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 import com.torrow.school.entity.TbUser;
 import com.torrow.school.util.PageBean;
+
 /**
  * 
  * @author 张金高
  *
- * 2017年10月6日下午4:37:24
+ *         2017年10月6日下午4:37:24
  */
 public interface TbUserService {
 
-    //登陆的方法
+	// 得到分页
+	PageBean<TbUser> findPage(int currentPage);
+
+	// 得到所有的用户
+	List<TbUser> selectAll();
+
+	// 根据id得到用户
+	TbUser selectById(int id);
+
+	// 根据id删除用户
+	int deleteById(int id);
+
+	// 添加用户
+	int addUser(TbUser tbUser);
+
+	// 修改用户
+	int updateByPrimaryKey(TbUser record);
+
+	// 上传图片
+	String uploadPicture(MultipartFile picture, String path) throws Exception;
+
+	// 这个方法是为了根据caId来查找的
+	TbUser selectByCaId(Integer caId);// 安李杰
+	// 这个方法根据caId来删除
+	int deleteByCaId(Integer caId);
+	// 登陆的方法
 	TbUser login(String usEmail, String usPassword);
 
-	//得到分页
-    PageBean<TbUser> findPage(int currentPage);
-    //得到所有的用户
-	List<TbUser> selectAll();
-	//根据id得到用户
-	TbUser selectById(int id);
-	//根据id删除用户
-	int deleteById(int id);
-	//添加用户
-	int addUser(TbUser tbUser);
-	//修改用户
-    int updateByPrimaryKey(TbUser record);
-    //上传图片
-	String uploadPicture(MultipartFile picture, String path)throws Exception;
-	
-	//这个方法是为了根据caId来查找的
-	TbUser selectByCaId(Integer caId);//安李杰
-	//这个方法根据caId来删除
-	int deleteByCaId(Integer caId);
-	
 }
